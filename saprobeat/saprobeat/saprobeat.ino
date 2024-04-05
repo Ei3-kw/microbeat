78dfcd8a7605301439662be6a1f2818ff3e87713
Ei3 <e013.kw@gmail.com>
2024-04-06 01:12:29 +1000

    Structure: project setup; Feature: midi2array

---
+++ saprobeat/saprobeat/saprobeat.ino
@@ -0,0 +1,51 @@
+#include <MIDI.h>
+#include <arduinoFFT.h>
+
+// MIDI instance
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+// FFT instance
+arduinoFFT FFT = arduinoFFT();
+
+// 2D array to store volume and pitch
+const int ARRAY_SIZE = 128;
+float volume[ARRAY_SIZE][2];
+float pitch[ARRAY_SIZE][2];
+
+void setup() {
+  // Initialize MIDI
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  MIDI.setHandleNoteOn(noteOnHandler);
+  MIDI.setHandleNoteOff(noteOffHandler);
+}
+
+void loop() {
+  // Read MIDI events
+  MIDI.read();
+}
+
+// MIDI note on event handler
+void noteOnHandler(byte channel, byte note, byte velocity) {
+  // Calculate volume and pitch
+  float vol = velocity / 127.0;
+  float freq = mtof(note);
+
+  // Store volume and pitch in the 2D array
+  volume[note][0] = vol;
+  volume[note][1] = millis();
+  pitch[note][0] = freq;
+  pitch[note][1] = millis();
+}
+
+// MIDI note off event handler
+void noteOffHandler(byte channel, byte note, byte velocity) {
+  // Clear the volume and pitch data for the note
+  volume[note][0] = 0.0;
+  volume[note][1] = 0.0;
+  pitch[note][1] = 0.0;
+}
+
+// MIDI note to frequency conversion
+float mtof(byte note) {
+  return pow(2, (note - 69) / 12.0) * 440;
+}
