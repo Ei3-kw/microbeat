6db604c8b2e36b0a2d150cc0341693b3f2c1b050
Ei3 <e013.kw@gmail.com>
2024-04-07 13:46:25 +1000

    final commit

--- phoebeat/src/phoebeat.cpp
+++ phoebeat/src/phoebeat.cpp
@@ -2,14 +2,10 @@
 #include <Arduino.h>
 
 const int NUM_SAMPLES = 256;
-const int SAMPLING_RATE = 6969;
+const int SAMPLING_RATE = 8000;
 
 float vReal[NUM_SAMPLES];
 float vImag[NUM_SAMPLES];
-
-// Vars to calculate BPM
-unsigned long lastTime = 0;
-unsigned long bpm = 0
 
 // Create FFT object
 ArduinoFFT<float> FFT = ArduinoFFT<float>(vReal, vImag, NUM_SAMPLES, SAMPLING_RATE);
@@ -41,10 +37,6 @@ void loop() {
     for (int i = 2; i < (NUM_SAMPLES / 2); i++) {
         double freq = (i * 1.0 * SAMPLING_RATE) / NUM_SAMPLES;
         double volume = vReal[i];
-        // Update BPM every 1s
-        if (millis() - lastTime >= 1000) {
-            calculateBPM();
-        }
         Serial.print(volume);
         Serial.print(",");
         Serial.println(freq);
@@ -53,10 +45,3 @@ void loop() {
   delay(100);
 }
 
-
-// Calculate the BPM based on the time between note on and note off events
-void calculateBPM() {
-    bpm = (unsigned long)(60000.0 / beatTimeDiff);
-    Serial.print(bpm);
-    Serial.print(",");
-}
--- saprobeat/serialDisplay.py
+++ saprobeat/serialDisplay.py
@@ -31,7 +31,7 @@ class Display(object):
         winsur = pygame.display.set_mode((width, height))
         font = pygame.font.SysFont('andalemono', 21)
         bg = pygame.Surface((width, height), pygame.SRCALPHA, 32).convert_alpha()
-        screen = pygame.di /splay.set_mode((width, height), pygame.FULLSCREEN) # full screen
+        screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN) # full screen
         winsur.fill((0, 0, 0))
 
         LETTER = '1234567890!@#$%^&*qwertyuiopasdfghjklzxcvbnm'
@@ -42,6 +42,16 @@ class Display(object):
         self.running = 1
         while self.running:
             texts = [font.render(LETTER[i], 1, (self.r, self.g, self.b)) for i in range(44)]
+
+            # Read data from serial
+            data = ser.readline().strip().decode()
+            try:
+                vol, freq = map(float, data.split(','))
+            except ValueError:
+                print(data)
+                continue
+
+            self.handle_update_interrupt(0, vol, freq)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -67,51 +77,41 @@ class Display(object):
     def handle_update_interrupt(self, bpm, vol, freq):
         if time.time() - self.last_update_time >= self.update_interval:
             self.last_update_time = time.time()
-            self.update(bpm, vol, freqs)
+            self.update(bpm, vol, freq)
 
-    def update(self):
-
+    def update(self, bpm, vol, freq):
         # fade [0, 256] <-> vol [0, 120]
-        self.fade = 256 - vol*32/15
+        # self.fade = int(256 - vol*32/15)
+        self.fade = int(256 - vol)%200
 
-        # delay [0, 69] <-> bpm [74, 220]
-        # f(196) = 6
-        # f(99) = 40
-        # f(91) = 50
-        # f(74) = 69
-        self.delay = .003 * (bpm-210)**2 + 5
-
-
-        # # rgb <-> freqs
-        # # low (< 246Hz)
-        # self.r = int(256*)
-        # # mid (246 ~ 2500Hz)
-        # self.g = int(256*)
-        # # hi (> 2500Hz)
-        # self.b = int(256)
+        # # delay [0, 69] <-> bpm [74, 220]
+        # # f(196) = 6
+        # # f(99) = 40
+        # # f(91) = 50
+        # # f(74) = 69
+        # self.delay = .003 * (bpm-210)**2 + 5
 
 
-        # # random
+        # random
         self.r = (self.r + random.randint(-Δ, Δ)) % 256
         self.g = (self.g + random.randint(-Δ, Δ)) % 256
         self.b = (self.b + random.randint(-Δ, Δ)) % 256
-        # self.delay = (self.delay + random.randint(-Δ, Δ)) % 69
+
+        # rgb <-> freqs
+        if freq < 246: # low (< 246Hz)
+            self.r = int(freq * 256 / 246)
+        elif 246 <= freq <2500: # mid (246 ~ 2500Hz)
+            self.g = int((freq-246) * 256 / 2254)
+        else: # hi (> 2500Hz)
+            self.b = int((freq-2500) * 256 / 7500)
+
+        self.delay = (self.delay + random.randint(-Δ, Δ)) % 69
         # self.fade = (self.fade + random.randint(-Δ, Δ)) % 200
 
 
 if __name__ == '__main__':
-    d = Display()
-    d.display()
     try:
-        while 1:
-            # Read data from serial
-            data = ser.readline().strip().decode()
-            vol, freq = map(float, data.split(','))
-
-            # Split data into volume and frequency
-            # bpm, vol, freq = map(float, data.split(','))
-
-            d.handle_update_interrupt(bpm, vol, freq)
+        Display().display()
 
     except KeyboardInterrupt:
         # Close serial connection when Ctrl+C is pressed
