59fbd84dcf42c34673b8a32963461eadccc11453
Jack O'Brien <jack.obrien2@uqconnect.edu.au>
2024-04-07 12:20:46 +1000

    SOund sensor now ouptuts smoothed volume estimate and spoofed fake frequency. Sent over serial

--- astilbeat/src/main.cpp
+++ astilbeat/src/main.cpp
@@ -1,6 +1,12 @@
 #define sensorPin 20
 
 #include "Arduino.h"
+#include <cmath>
+
+float volumeEstimate = 0;
+float alpha = 0.5   // Smoothing factor
+int time = 0 //
+int freq;
 
 void setup() {
 	pinMode(sensorPin, INPUT);
@@ -8,7 +14,16 @@ void setup() {
 }
 
 void loop() {
+	// Avg volume
 	int soundValue = analogRead(sensorPin);
-	Serial.println(soundValue);
-
+	volumeEstimate += alpha * soundValue / (1+ alpha);
+	
+	// Spoof frequency using a sin curve
+	// Units of frequency are Hz
+	// We want freq in 100 - 300hz
+	freq = 100 + sin(time) * 100;
+	
+	Serial.print(volumeEstimate);
+	Serial.print(",");
+	Serial.println(freq);
 }
